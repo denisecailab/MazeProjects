eb5d2838eb192e20348c851081db2c37a527bd81
phildong <zhe.dong@icahn.mssm.edu>
2022-06-07 21:26:05 -0400

    add scripts to compute PV corr

---
+++ 02.compute_PV_corr.py
@@ -0,0 +1,18 @@
+#%% imports
+import pickle as pkl
+import warnings
+
+from routine.CircleTrack.RecentReversal import RecentReversal
+
+warnings.filterwarnings("ignore")
+
+IN_ANM = ["Janus", "PhantomLyra"]
+IN_DB = "./intermediate/database.sqlite"
+OUT_PV_CORR = "./intermediate/pv_corr.pkl"
+#%% compute pv corr
+RR = RecentReversal(
+    IN_ANM, db_fname=IN_DB, project_name="RemoteReversal", behavior_only=False
+)
+pv_corr = RR.PV_corr_all_mice()
+with open(OUT_PV_CORR, "wb") as pvf:
+    pkl.dump(pv_corr, pvf)
